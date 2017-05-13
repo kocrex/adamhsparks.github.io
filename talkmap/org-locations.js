4d70fffad1a6b97b682c34be4bcf7c97564f0204
adamhsparks <adamhsparks@gmail.com>
2017-05-13 21:05:01 +1000

    Update talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -3,5 +3,45 @@ var addressPoints = [
     "Pyeongchang, Gangwon-do, Korea", 
     37.5511821, 
     128.4471608
+  ], 
+  [
+    "Bangkok, Thailand", 
+    13.7538929, 
+    100.8160803
+  ], 
+  [
+    "Ho Chi Minh City, Vietnam", 
+    10.7758439, 
+    106.7017555
+  ], 
+  [
+    "Geneva, NY, USA", 
+    42.8689552, 
+    -76.9777435
+  ], 
+  [
+    "Providence, Rhode Island", 
+    41.8239891, 
+    -71.4128342
+  ], 
+  [
+    "Beijing, China", 
+    39.9059631, 
+    116.391248
+  ], 
+  [
+    "Auckland, New Zealand", 
+    -36.8534664, 
+    174.7655514
+  ], 
+  [
+    "Fremantle, WA, Australia", 
+    -32.0520329, 
+    115.74527
+  ], 
+  [
+    "Raipur, Chhattisgarh, India", 
+    21.2379468, 
+    81.6336833
   ]
 ];
