22c62f45e772337be7ffd2a7184caa3f0bdd8929
adamhsparks <adamhsparks@gmail.com>
2017-05-13 21:10:43 +1000

    Update talks and talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,13 @@
 var addressPoints = [
   [
-    "Pyeongchang, Gangwon-do, Korea", 
-    37.5511821, 
-    128.4471608
+    "Geneva, NY, USA", 
+    42.8689552, 
+    -76.9777435
+  ], 
+  [
+    "New York, New York, USA", 
+    40.7305991, 
+    -73.9865811
   ], 
   [
     "Bangkok, Thailand", 
@@ -10,34 +15,39 @@ var addressPoints = [
     100.8160803
   ], 
   [
-    "Ho Chi Minh City, Vietnam", 
-    10.7758439, 
-    106.7017555
-  ], 
-  [
-    "Geneva, NY, USA", 
-    42.8689552, 
-    -76.9777435
-  ], 
-  [
-    "Providence, Rhode Island", 
-    41.8239891, 
-    -71.4128342
-  ], 
-  [
     "Beijing, China", 
     39.9059631, 
     116.391248
   ], 
   [
+    "Sydney, New South Wales, Australia", 
+    -33.8548156, 
+    151.2164539
+  ], 
+  [
+    "Providence, Rhode Island, USA", 
+    41.8239891, 
+    -71.4128342
+  ], 
+  [
+    "Pyeongchang, Gangwon-do, Korea", 
+    37.5511821, 
+    128.4471608
+  ], 
+  [
+    "Fremantle, Western Australia, Australia", 
+    -32.0520329, 
+    115.74527
+  ], 
+  [
+    "Ho Chi Minh City, Vietnam", 
+    10.7758439, 
+    106.7017555
+  ], 
+  [
     "Auckland, New Zealand", 
     -36.8534664, 
     174.7655514
-  ], 
-  [
-    "Fremantle, WA, Australia", 
-    -32.0520329, 
-    115.74527
   ], 
   [
     "Raipur, Chhattisgarh, India", 
