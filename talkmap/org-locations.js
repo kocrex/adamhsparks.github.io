c3d5916f1d53bb688d735ea5cd648f8d114cd9d4
Adam H. Sparks <adamhsparks@gmail.com>
2017-09-30 22:11:54 +1000

    Add Brisbane talk to map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,67 +1,72 @@
 var addressPoints = [
   [
-    "Geneva, NY, USA",
-    42.8689552,
+    "Geneva, NY, USA", 
+    42.8689552, 
     -76.9777436
-  ],
+  ], 
   [
-    "New York, New York, USA",
-    40.7306458,
+    "New York, New York, USA", 
+    40.7306458, 
     -73.9866136
-  ],
+  ], 
   [
-    "Providence, Rhode Island, USA",
-    41.8239891,
-    -71.4128343
-  ],
-  [
-    "Auckland, New Zealand",
-    -36.8534665,
-    174.7655514
-  ],
-  [
-    "Sydney, New South Wales, Australia",
-    -33.8548157,
-    151.2164539
-  ],
-  [
-    "Los Ba\u00f1os, Calabarzon, Philippines",
-    14.15550365,
-    121.225013458769
-  ],
-  [
-    "Beijing, China",
-    39.9059631,
-    116.391248
-  ],
-  [
-    "Raipur, Chhattisgarh, India",
-    21.2379468,
-    81.6336833
-  ],
-  [
-    "Ho Chi Minh City, Vietnam",
-    10.7758439,
-    106.7017555
-  ],
-  [
-    "Bangkok, Thailand",
-    13.7542529,
+    "Bangkok, Thailand", 
+    13.7542529, 
     100.493087
-  ],
+  ], 
   [
-    "Fremantle, Western Australia, Australia",
-    -32.052033,
-    115.74527
-  ],
+    "Beijing, China", 
+    39.9059631, 
+    116.391248
+  ], 
   [
-    "Pyeongchang, Gangwon-do, Korea",
-    37.3736539,
+    "Sydney, New South Wales, Australia", 
+    -33.8548157, 
+    151.2164539
+  ], 
+  [
+    "Providence, Rhode Island, USA", 
+    41.8239891, 
+    -71.4128343
+  ], 
+  [
+    "Brisbane, Queensland, Australia", 
+    -27.4689682, 
+    153.0234991
+  ], 
+  [
+    "Pyeongchang, Gangwon-do, Korea", 
+    37.3736539, 
     128.3883829
-  ],
+  ], 
   [
-    "Jondaryan, Queensland, Australia",
-    -27.3689172,
+    "Jondaryan, Queensland, Australia", 
+    -27.3689172, 
     151.5922633
+  ], 
+  [
+    "Fremantle, Western Australia, Australia", 
+    -32.052033, 
+    115.74527
+  ], 
+  [
+    "Ho Chi Minh City, Vietnam", 
+    10.7758439, 
+    106.7017555
+  ], 
+  [
+    "Auckland, New Zealand", 
+    -36.8534665, 
+    174.7655514
+  ], 
+  [
+    "Raipur, Chhattisgarh, India", 
+    21.2379468, 
+    81.6336833
+  ], 
+  [
+    "Los Ba\u00f1os, Calabarzon, Philippines", 
+    14.15550365, 
+    121.225013458769
   ]
 ];
